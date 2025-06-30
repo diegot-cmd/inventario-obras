18561d2c8bd4df37a6e82984423c85f073cd5981
Diego Ricardo Torres Campos <dtorresca@autonoma.edu.pe>
2025-06-29 21:27:55 -0500

    feat: cambios en rutas y formularios

---
+++ src/app/materiales/editar/page.tsx
@@ -0,0 +1,19 @@
+// src/app/materiales/editar/[id]/page.tsx
+'use client'
+
+import { useParams } from 'next/navigation'
+import EditarMaterialForm from '@/app/components/EditarMaterialform'
+
+export default function EditarMaterialPage() {
+  const params = useParams()
+  const id = Number(params.id)
+
+  if (!id) return <p>ID inválido</p>
+
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Editar Material</h1>
+      <EditarMaterialForm editId={id} />
+    </div>
+  )
+}
